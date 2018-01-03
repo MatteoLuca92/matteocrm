82126dc838aeb6ed370c2fada809c3e69be20e27
MatteoLuca <imazzrecords@gmail.com>
2018-01-03 15:18:09 +0100

    aggiunta table photo

--- app/Photo.php
+++ app/Photo.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     //
+
+
+
+    protected $fillable = ['file'];
+
+
+
+
 }
--- app/User.php
+++ app/User.php
@@ -29,4 +29,12 @@ class User extends Authenticatable
 return $this->belongsTo('App\Role');
 
     }
+
+
+    public function  photo(){
+
+        return $this->belongsTo('App\Photo');
+    }
+
+
 }
